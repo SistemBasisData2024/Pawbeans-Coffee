6dc7286fec0f6af4efe67445a4f1e9a17be879f1
Aisyah Arifatul Alya <arifatulalya417e@gmail.com>
2024-06-07 19:41:23 +0700

    update table for cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -2,13 +2,13 @@ const pool = require('../db/db');
 
 const addToCart = async (req, res) => {
     try {
-        const { userId, itemId } = req.body;
+        const { user_id, coffee_id } = req.body;
         await pool.query(
-            `INSERT INTO cart_items (user_id, item_id, quantity)
+            `INSERT INTO cart_items (user_id, coffee_id, quantity)
              VALUES ($1, $2, 1)
-             ON CONFLICT (user_id, item_id) DO UPDATE
+             ON CONFLICT (user_id, coffee_id) DO UPDATE
              SET quantity = cart_items.quantity + 1`,
-            [userId, itemId]
+            [user_id, coffee_id]
         );
         res.json({ success: true, message: 'Added To Cart' });
     } catch (error) {
@@ -19,18 +19,18 @@ const addToCart = async (req, res) => {
 
 const removeFromCart = async (req, res) => {
     try {
-        const { userId, itemId } = req.body;
+        const { user_id, coffee_id } = req.body;
         await pool.query(
             `UPDATE cart_items
              SET quantity = quantity - 1
-             WHERE user_id = $1 AND item_id = $2 AND quantity > 0`,
-            [userId, itemId]
+             WHERE user_id = $1 AND coffee_id = $2 AND quantity > 0`,
+            [user_id, coffee_id]
         );
-        // Optionally, delete item if quantity drops to 0
+        // Deletes the item from the cart if quantity is equal or less than zero
         await pool.query(
             `DELETE FROM cart_items
-             WHERE user_id = $1 AND item_id = $2 AND quantity <= 0`,
-            [userId, itemId]
+             WHERE user_id = $1 AND coffee_id = $2 AND quantity <= 0`,
+            [user_id , coffee_id]
         );
         res.json({ success: true, message: 'Removed From Cart' });
     } catch (error) {
@@ -41,12 +41,12 @@ const removeFromCart = async (req, res) => {
 
 const getCart = async (req, res) => {
     try {
-        const { userId } = req.body;
+        const { user_id  } = req.body;
         const result = await pool.query(
             `SELECT item_id, quantity
              FROM cart_items
              WHERE user_id = $1`,
-            [userId]
+            [user_id]
         );
         res.json({ success: true, cartData: result.rows });
     } catch (error) {
